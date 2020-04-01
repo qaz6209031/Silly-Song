98520fd3b297a02ec15202fd913a4666885cb198
Kai-Chin Huang <qaz6209031@gmail.com>
2020-04-01 11:25:36 +0800

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Kai-Chin Huang on 4/1/20.
+//  Copyright © 2020 Kai-Chin Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
